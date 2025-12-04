a05ae7f7520535dc8c769b6061438445cc662910
Blonded <lmartinson20@gmail.com>
2025-12-03 23:36:21 -0800

    Read and print players choice

--- rps.c
+++ rps.c
@@ -1,7 +1,19 @@
 // Simple rock paper scissors game in C
 #include <stdio.h>
+#include <string.h>
 
 int main(void){
-    printf("Welcome to the Rock Paper Scissors Game! ");
+    char player_choice[20];
+    printf("Welcome to the Rock Paper Scissors Game! \n");
+    printf("Type rock, paper, or scissors: ");
+
+    // take users input
+    if(scanf("%19s", player_choice) != 1){
+        printf("Input error.\n");
+        return 1;
+    }
+
+    printf("You chose: %s\n", player_choice);
+
     return 0;
 }
