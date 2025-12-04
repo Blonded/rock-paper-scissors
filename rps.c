21cf538095c6a9b47105d5d02b4a3828b21d41cb
Blonded <lmartinson20@gmail.com>
2025-12-03 23:45:15 -0800

    added int values to user choice

--- rps.c
+++ rps.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <string.h>
 
+// helper function for main
+int choice_to_code(const char *choice){
+    if(strcmp(choice, "rock") ==0) {
+        return 0;
+    } else if (strcmp(choice, "paper") == 0){
+        return 1;
+    } else if (strcmp(choice, "scissors") == 0){
+        return 2;
+    } else {
+        return -1;
+    }
+}
+
 int main(void){
     char player_choice[20];
     printf("Welcome to the Rock Paper Scissors Game! \n");
@@ -13,7 +26,16 @@ int main(void){
         return 1;
     }
 
-    printf("You chose: %s\n", player_choice);
+    int player_code = choice_to_code(player_choice);
+
+    if (player_code == -1){
+        printf("Invalid choice: %s\n", player_choice);
+        return 1;
+    }
+
+    // printf("You chose: %s\n", player_choice);
+    printf("You chose: %s (code %d)\n", player_choice, player_code);
+
 
     return 0;
 }
